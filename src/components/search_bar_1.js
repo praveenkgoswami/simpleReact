2f40376e0afa04965c14f9bd7acbe827578749de
Praveen-Kumar <praveen.kumar@tothenew.com>
2018-05-07 17:01:33 +0530

    index update

--- src/components/search_bar_1.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class SearchBarEvent extends Component {
-  render(){
-    return <input onChange={event => console.log(event.target.value)} />;
-  }
-}
-
-export default SearchBarEvent;
